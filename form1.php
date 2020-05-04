cf39b0ac86965e20329acd692246458c4268cc5f
Aryan Sharma <62653288+Aryan06@users.noreply.github.com>
2020-05-04 17:02:04 +0530

    Add files via upload

---
+++ form1.php
@@ -0,0 +1,23 @@
+<html>
+<body>
+
+<form action="data.php" method="post">
+
+<strong><em> INTRODUCTION </em></strong>
+<br>
+<br>
+
+<strong>Name:</strong> <input type="text" name="name"><br>
+<br>
+
+<strong>Title:</strong> <input type="text" name="name"><br>
+<br>
+
+<strong> Designation:</strong> <input type="text" name="name"><br>
+<br>
+
+<input type="Submit">
+
+</form>
+</body>
+</html>
